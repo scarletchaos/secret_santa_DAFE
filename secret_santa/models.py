809acef662d3fd2a794eb3a9f1f30b302baaab15
Georgy <bachakashvili.ga@gmail.com>
2024-04-07 13:12:32 +0300

    добавил поле с текущим приоритетом

--- secret_santa/models.py
+++ secret_santa/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    telegram_id = models.PositiveIntegerField(unique=True, blank=True, null=True)
+    telegram_id = models.PositiveIntegerField(
+        unique=True, blank=True, null=True)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     wishes = models.CharField(max_length=100)
@@ -11,7 +12,8 @@ class User(models.Model):
     year = models.PositiveIntegerField(null=True, blank=True)
     gifts_to = models.IntegerField(null=True, unique=True, blank=True)
     has_giver = models.BooleanField(default=False)
-    
+    current_priority = models.IntegerField(default=0)
+
     def __str__(self):
         return f"{self.name} {self.surname}"
 # Create your models here.
